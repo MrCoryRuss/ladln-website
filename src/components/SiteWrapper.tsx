12fe6fcec8a0e04844726f0aff300b5bb2adb72a
MrCoryRuss <145234854+MrCoryRuss@users.noreply.github.com>
2026-07-11 07:51:46 -0500

    Refactor SiteWrapper to include Students component

--- src/components/SiteWrapper.tsx
+++ src/components/SiteWrapper.tsx
@@ -4,27 +4,30 @@ import { Lang } from "@/lib/i18n";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import About from "./About";
+import Students from "./Students";
+import News from "./News";
 import Donate from "./Donate";
 import Membership from "./Membership";
-import News from "./News";
 import Volunteer from "./Volunteer";
 import Footer from "./Footer";
 
 export default function SiteWrapper() {
-  const [lang, setLang] = useState<Lang>("en");
+    const [lang, setLang] = useState<Lang>("en");
 
   return (
-    <>
-      <Navbar lang={lang} setLang={setLang} />
-      <main>
-        <Hero lang={lang} />
-        <News lang={lang} />
-        <About lang={lang} />
-        <Donate lang={lang} />
-        <Membership lang={lang} />
-        <Volunteer lang={lang} />
-      </main>
-      <Footer lang={lang} />
-    </>
-  );
+        <>
+              <Navbar lang={lang} setLang={setLang} />
+              <main>
+                      <Hero lang={lang} />
+                      <About lang={lang} />
+                      <Students lang={lang} />
+                      <News lang={lang} />
+                      <Donate lang={lang} />
+                      <Membership lang={lang} />
+                      <Volunteer lang={lang} />
+              </main>main>
+              <Footer lang={lang} />
+        </>>
+      );
 }
+</>
